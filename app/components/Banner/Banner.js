1e2d1aae16f8b9863fa31a2a64f5346ae170a508
Ryan <rmthomas1998@gmail.com>
2022-04-10 22:18:06 -0400

    animations

--- app/components/Banner/Banner.js
+++ app/components/Banner/Banner.js
@@ -1,22 +1,31 @@
 import styles from "./Banner.module.css";
 import { Link } from "react-scroll";
+import { Fade } from "react-awesome-reveal";
 
 const Banner = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <p className={styles.header}>
-          Built for Traders<br></br>By Traders
-        </p>
-        <p className={styles.subHeader}>Premier Day Trading Co-Working Space</p>
-        <div className={styles.buttonContainer}>
-          <Link to="apply" smooth className={styles.applyBtn}>
-            Apply Now
-          </Link>
-          <Link to="what-we-offer" smooth className={styles.learnMoreBtn}>
-            Learn More
-          </Link>
-        </div>
+        <Fade direction="up" delay={200}>
+          <p className={styles.header}>
+            Built for Traders<br></br>By Traders
+          </p>
+        </Fade>
+        <Fade direction="up" delay={1000}>
+          <p className={styles.subHeader}>
+            Premier Day Trading Co-Working Space
+          </p>
+        </Fade>
+        <Fade direction="up" delay={1200}>
+          <div className={styles.buttonContainer}>
+            <Link to="apply" smooth className={styles.applyBtn}>
+              Apply Now
+            </Link>
+            <Link to="what-we-offer" smooth className={styles.learnMoreBtn}>
+              Learn More
+            </Link>
+          </div>
+        </Fade>
       </div>
     </div>
   );
