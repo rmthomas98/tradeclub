0bbab33e985add5b9cd4ea1c8e3a4010b9296518
Ryan <rmthomas1998@gmail.com>
2022-04-11 19:51:46 -0400

    update

--- app/components/Apply/Apply.js
+++ app/components/Apply/Apply.js
@@ -1,7 +1,10 @@
 import styles from "./Apply.module.css";
-import {Fade} from 'react-awesome-reveal';
+import { Fade } from "react-awesome-reveal";
+import { useWindowWidth } from "@react-hook/window-size";
 
 const Apply = () => {
+  const width = useWindowWidth();
+
   return (
     <div className={styles.wrapper} id="apply">
       <form className={styles.container}>
@@ -10,85 +13,88 @@ const Apply = () => {
           Apply below for a Trading Desk. First come, first serve!
         </p>
         <Fade direction="up">
-        <div className={styles.nameContainer}>
-          <div className={styles.inputContainer} style={{ marginRight: 20 }}>
-            <p className={styles.label}>First Name</p>
+          <div className={styles.nameContainer}>
+            <div className={styles.inputContainer} style={{ marginRight: 20 }}>
+              <p className={styles.label}>First Name</p>
+              <input
+                type="text"
+                className={styles.input}
+                placeholder="First Name"
+              />
+            </div>
+            <div className={styles.inputContainer}>
+              <p className={styles.label}>Last Name</p>
+              <input
+                type="text"
+                className={styles.input}
+                placeholder="Last Name"
+              />
+            </div>
+          </div>
+          <div className={styles.inputContainer}>
+            <p className={styles.label}>Email</p>
+            <input type="email" placeholder="Email" className={styles.input} />
+          </div>
+          <div className={styles.flexContainer}>
+            <div className={styles.inputContainer} style={{ marginRight: 20 }}>
+              <p className={styles.label}>Phone</p>
+              <input className={styles.input} placeholder="Phone Number" />
+            </div>
+            <div className={styles.inputContainer}>
+              <p className={styles.label}>How long have you been trading?</p>
+              <select className={styles.input}>
+                <option>I haven&#39;t began trading yet</option>
+                <option>Less than 1 year</option>
+                <option>1-2 Years</option>
+                <option>3-4 Years</option>
+                <option>5+ Years</option>
+              </select>
+            </div>
+          </div>
+          <div className={styles.inputContainer}>
+            <p className={styles.label}>
+              In what market do you have experience trading?
+            </p>
             <input
               type="text"
               className={styles.input}
-              placeholder="First Name"
+              placeholder="(Stocks, Crypto, Futures, FX, etc.)"
             />
           </div>
           <div className={styles.inputContainer}>
-            <p className={styles.label}>Last Name</p>
+            <p className={styles.label}>Where are you currently based?</p>
             <input
               type="text"
               className={styles.input}
-              placeholder="Last Name"
+              placeholder="(City, State, Country)"
             />
           </div>
-        </div>
-        <div className={styles.inputContainer}>
-          <p className={styles.label}>Email</p>
-          <input type="email" placeholder="Email" className={styles.input} />
-        </div>
-        <div className={styles.flexContainer}>
-          <div className={styles.inputContainer} style={{ marginRight: 20 }}>
-            <p className={styles.label}>Phone</p>
-            <input className={styles.input} placeholder="Phone Number" />
-          </div>
           <div className={styles.inputContainer}>
-            <p className={styles.label}>How long have you been trading?</p>
+            <p className={styles.label}>
+              Please explain your interest in Trade Club
+            </p>
+            <textarea
+              className={styles.textarea}
+              placeholder="Write a short message explaining your interest in Trade Club."
+            />
+          </div>
+          <div
+            className={styles.inputContainer}
+            style={{ maxWidth: width > 500 ? 300 : 500 }}
+          >
+            <p className={styles.label}>Desired Membership Length?</p>
             <select className={styles.input}>
-              <option>I haven&#39;t began trading yet</option>
-              <option>Less than 1 year</option>
-              <option>1-2 Years</option>
-              <option>3-4 Years</option>
-              <option>5+ Years</option>
+              <option>1 Week / $997</option>
+              <option>3 Months / $2999 Per Month</option>
+              <option>6 Months / $2499 Per Month</option>
+              <option>12 Months / $1999 Per Month</option>
             </select>
           </div>
-        </div>
-        <div className={styles.inputContainer}>
-          <p className={styles.label}>
-            In what market do you have experience trading?
-          </p>
-          <input
-            type="text"
-            className={styles.input}
-            placeholder="(Stocks, Crypto, Futures, FX, etc.)"
-          />
-        </div>
-        <div className={styles.inputContainer}>
-          <p className={styles.label}>Where are you currently based?</p>
-          <input
-            type="text"
-            className={styles.input}
-            placeholder="(City, State, Country)"
-          />
-        </div>
-        <div className={styles.inputContainer}>
-          <p className={styles.label}>
-            Please explain your interest in Trade Club
-          </p>
-          <textarea
-            className={styles.textarea}
-            placeholder="Write a short message explaining your interest in Trade Club."
-          />
-        </div>
-        <div className={styles.inputContainer} style={{ maxWidth: 300 }}>
-          <p className={styles.label}>Desired Membership Length?</p>
-          <select className={styles.input}>
-            <option>1 Week / $997</option>
-            <option>3 Months / $2999 Per Month</option>
-            <option>6 Months / $2499 Per Month</option>
-            <option>12 Months / $1999 Per Month</option>
-          </select>
-        </div>
-        <div className={styles.buttonContainer}>
-          <button type="submit" className={styles.submitBtn}>
-            Submit
-          </button>
-        </div>
+          <div className={styles.buttonContainer}>
+            <button type="submit" className={styles.submitBtn}>
+              Submit
+            </button>
+          </div>
         </Fade>
       </form>
     </div>
